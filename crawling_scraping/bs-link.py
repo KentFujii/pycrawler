29788e69a57c06180d5d16a3cffdf6052d39bf4a
kent fujii <kent.where.the.light.is@gmail.com>
2017-06-15 02:08:10 +0900

    add: beautifulsoup

--- crawling_scraping/bs-link.py
+++ crawling_scraping/bs-link.py
@@ -2,5 +2,15 @@ from bs4 import BeautifulSoup
 
 html = '''
 <html><body>
+    <li><a href="http://uta.pw">uta</a></li>
+    <li><a href="http://oto.chu.jp">oto</a></li>
 </body></html>
 '''
+
+soup = BeautifulSoup(html, 'html.parser')
+links = soup.find_all("a")
+
+for a in links:
+    href = a.attrs['href']
+    text = a.string
+    print(text, ">", href)
--- readme.md
+++ readme.md
@@ -4,6 +4,8 @@ sandbox for collective intelligence engineering with python
 
 ## setup
 
+```
 python -m venv env
 source env/bin/activate
 pip install -r requirements.txt
+```
