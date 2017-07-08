5758a4227c8299c81a08a30db5b91567ecf0edc1
kent fujii <kent.where.the.light.is@gmail.com>
2017-07-08 15:07:46 +0900

    add: train_test_split

---
+++ ml/iris-train2.py
@@ -0,0 +1,22 @@
+from os import path
+import pandas as pd
+from sklearn import svm, metrics
+from sklearn.model_selection import train_test_split
+
+target_path = path.dirname(__file__) + '/csv/iris.csv'
+csv = pd.read_csv(target_path)
+
+csv_data = csv[
+    ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth']
+]
+csv_label = csv['Name']
+
+train_data, test_data, train_label, test_label = train_test_split(csv_data,
+                                                                  csv_label)
+
+clf = svm.SVC()
+clf.fit(train_data, train_label)
+pre = clf.predict(test_data)
+
+ac_score = metrics.accuracy_score(test_label, pre)
+print("正解率 =", ac_score)
