4be80a61057a9c8613c2a0043db1382d3704f79e
kent fujii <kent.where.the.light.is@gmail.com>
2017-07-08 14:06:25 +0900

    add: svm_irris

--- ml/iris-train.py
+++ ml/iris-train.py
@@ -1,6 +1,7 @@
 from os import path
+import random
+import re
 from sklearn import svm, metrics
-import random, re
 
 
 def str_to_num(num):
@@ -21,3 +22,29 @@ with open(target_path, 'r', encoding='utf-8') as fp:
 
 del csv[0]
 print(csv)
+
+random.shuffle(csv)
+
+total_len = len(csv)
+train_len = int(total_len * 2 / 3)
+train_data = []
+train_label = []
+test_data = []
+test_label = []
+
+for i in range(total_len):
+    data = csv[i][0:4]
+    label = csv[i][4]
+    if i < train_len:
+        train_data.append(data)
+        train_label.append(label)
+    else:
+        test_data.append(data)
+        test_label.append(label)
+
+clf = svm.SVC()
+clf.fit(train_data, train_label)
+pre = clf.predict(test_data)
+
+ac_score = metrics.accuracy_score(test_label, pre)
+print("正解率 =", ac_score)
