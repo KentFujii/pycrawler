62b847451d7a5034928b27077e504e5e64e69b7f
kent fujii <kent.where.the.light.is@gmail.com>
2017-09-30 23:06:46 +0900

    minor_fix

--- tensorflow/bmi.py
+++ tensorflow/bmi.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 
 csv = pd.read_csv('bmi.csv')
 csv['height'] = csv['height'] / 200
-csv['weight'] = csv['height'] / 100
+csv['weight'] = csv['weight'] / 100
 
 bclass = {'thin': [1, 0, 0], 'normal': [0, 1, 0], 'fat': [0, 0, 1]}
 csv['label_pat'] = csv['label'].apply(lambda x: np.array(bclass[x]))
@@ -28,12 +28,11 @@ predict = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(predict, tf.float32))
 
 sess = tf.Session()
-# tw = tf.summary.FileWriter("log_dir", graph=sess.graph)
 sess.run(tf.global_variables_initializer())
 
 for step in range(3500):
     i = (step * 100) % 14000
-    rows = csv[1 + i : 1 + i + 100]
+    rows = csv[(1 + i):(1 + i + 100)]
     x_pat = rows[['weight', 'height']]
     y_ans = list(rows['label_pat'])
     fd = {x: x_pat, y_: y_ans}
